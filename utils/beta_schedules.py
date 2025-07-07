6eb0347488742db8bbed2164aeb9e45945eafcd8
louise.wiljander <louisewiljander@live.com>
2025-07-07 14:39:56 +0200

    Adjusted beta annealing schedule to add warmup and cool-down

--- utils/beta_schedules.py
+++ utils/beta_schedules.py
@@ -5,9 +5,16 @@ Utility functions for beta scheduling in Beta-VAE training.
 """
 
 def linear_beta_schedule(epoch, total_epochs, beta_start=5.0, beta_end=1.0):
-    """Linearly anneal beta from beta_start to beta_end over total_epochs."""
-    if total_epochs == 0:
+    """
+    Linearly anneal beta from beta_start to beta_end over total_epochs,
+    but keep beta fixed at beta_start for the first epoch (epoch 0).
+    The last epoch (epoch=total_epochs-1) will use beta_end.
+    """
+    if total_epochs <= 1:
         return beta_end
-    if epoch >= total_epochs:
+    if epoch == 0:
+        return beta_start
+    if epoch >= total_epochs - 1:
         return beta_end
-    return beta_start + (beta_end - beta_start) * (epoch / total_epochs)
+    # Anneal from epoch 1 to total_epochs-2
+    return beta_start + (beta_end - beta_start) * ((epoch - 1) / (total_epochs - 2))
