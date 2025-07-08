bc258ef949c8597dd15b97545594d806f8ccabbe
louise.wiljander <louisewiljander@live.com>
2025-07-08 14:00:02 +0200

    Experiment reconstruction script for dsprites

--- run_experiments_dsprites.sh
+++ run_experiments_dsprites.sh
@@ -6,17 +6,17 @@ LATENT_DIM=10
 EPOCHS=10
 DATASET=dsprites
 
+# Fixed Beta=10
+python main.py --beta_start 10 --beta_end 10 --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
+
+# Annealed Beta=10->1
+python main.py --beta_start 10 --beta_end 1 --beta_anneal_epochs $EPOCHS --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
+
 # Fixed Beta=4
 python main.py --beta_start 4 --beta_end 4 --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
 
 # Annealed Beta=4->1
 python main.py --beta_start 4 --beta_end 1 --beta_anneal_epochs $EPOCHS --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
-
-# Fixed Beta=8
-python main.py --beta_start 8 --beta_end 8 --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
-
-# Annealed Beta=8->1
-python main.py --beta_start 8 --beta_end 1 --beta_anneal_epochs $EPOCHS --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
 
 # Fixed Beta=0.1
 python main.py --beta_start 0.1 --beta_end 0.1 --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
@@ -25,4 +25,4 @@ python main.py --beta_start 0.1 --beta_end 0.1 --epochs $EPOCHS --latent-dim $LA
 python main.py --beta_start 0.1 --beta_end 1 --beta_anneal_epochs $EPOCHS --epochs $EPOCHS --latent-dim $LATENT_DIM --dataset $DATASET --seed $SEED
 
 # Plotting the results
-python plot_experiment_results.py
+#python plot_experiment_results.py
